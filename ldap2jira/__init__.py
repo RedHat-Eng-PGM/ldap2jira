96db76b42a2f1e2d4c5ed2e44876644f5256f2b6
Pavel Slama <pslama@redhat.com>
2021-03-08 16:59:08 +0100

    Fix line length

--- ldap2jira/__init__.py
+++ ldap2jira/__init__.py
@@ -1,2 +1,5 @@
-from ldap2jira.ldap_lookup import LDAPLookup, LDAPQueryNotFoundError  # noqa: F401
+from ldap2jira.ldap_lookup import (  # noqa: F401
+    LDAPLookup,
+    LDAPQueryNotFoundError
+)
 from ldap2jira.map import LDAP2JiraUserMap  # noqa: F401
